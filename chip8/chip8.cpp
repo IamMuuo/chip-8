1479c1a4dab738d1f694ad2afce916dd1e338419
Erick <hearteric57@gmail.com>
2023-04-11 21:54:26 +0300

    Got the CHIP8 to  initialize font and keypad

--- chip8/chip8.cpp
+++ chip8/chip8.cpp
@@ -1,36 +1,72 @@
 #include "chip8.hpp"
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
 
 #define START_ADDRESS 0x200
+#define FONTSET_START_ADDRESS 0x50
+
 // contains implementation of various fuctions
 // to be used by the chip8 class
 
-void Chip8::loadROM(const char* filename)
-{
-    // open the file and navigate to end
-    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+Chip8::Chip8() {
+  // set the program counter to 0x200 so it starts executing
+  // from there
 
-    if(file.is_open())
-    {
-        // get the size of the file
-        std::streampos size = file.tellg();
+  programCounter = START_ADDRESS;
 
-        char* buffer = new char[size];
+  // initialize the fonts
+  const unsigned int FONTSET_SIZE = 80;
 
+  uint8_t fontset[FONTSET_SIZE] = {
+      0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+      0x20, 0x60, 0x20, 0x20, 0x70, // 1
+      0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+      0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+      0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+      0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+      0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+      0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+      0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+      0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+      0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+      0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+      0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+      0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+      0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+      0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+  };
+  // Load fonts into memory
+  for (unsigned int i = 0; i < FONTSET_SIZE; ++i) {
+    memory[FONTSET_START_ADDRESS + i] = fontset[i];
+  }
 
-        // get to the beginning of the file filling the memory buffer
-        file.seekg(0,std::ios::beg);
-        file.read(buffer, size);
-        file.close();
+  // set a random value to randbyte
+  srand(time(nullptr));
+  randByte = rand() % 255;
+}
 
-        // Load the instructions read to the CHIP's memory
-        for(auto i =0;i<size; i++)
-        {
-            memory[START_ADDRESS + i] = buffer[i];
-        }
+void Chip8::loadROM(const char *filename) {
+  // open the file and navigate to end
+  std::ifstream file(filename, std::ios::binary | std::ios::ate);
 
-        // delete the buffer
-        delete[] buffer;
+  if (file.is_open()) {
+    // get the size of the file
+    std::streampos size = file.tellg();
 
+    char *buffer = new char[size];
+
+    // get to the beginning of the file filling the memory buffer
+    file.seekg(0, std::ios::beg);
+    file.read(buffer, size);
+    file.close();
+
+    // Load the instructions read to the CHIP's memory
+    for (auto i = 0; i < size; i++) {
+      memory[START_ADDRESS + i] = buffer[i];
     }
+
+    // delete the buffer
+    delete[] buffer;
+  }
 }
--- chip8/chip8.hpp
+++ chip8/chip8.hpp
@@ -1,39 +1,39 @@
 /**********************************************************************
  * (C) Copyright 2023
  * All rights reserved
- * 
+ *
  * Author: Erick Muuo
  *
  * Project Chip8
  *
  * *******************************************************************/
-
-
+#include <cstdint>
 // A class to represent the chip8 core architecture
 
-struct Chip8
-{
-    public:
-        uint8_t registers[16];  // The 16 8-bit registers 0-F
-        uint8_t memory[4096];   // 4KB of RAM
-        uint16_t index;         // A single 16-bit register to store memory 
-                                // used in various operations
+struct Chip8 {
+public:
+  uint8_t registers[16]; // The 16 8-bit registers 0-F
+  uint8_t memory[4096];  // 4KB of RAM
+  uint16_t index;        // A single 16-bit register to store memory
+                         // used in various operations
 
-        uint16_t programCounter; // stores the memory address of the statement 
-                                 // to be executed
-        
-        uint16_t stack[16];     // the 16 level stack
-        uint8_t stackPointer;   // 8-bit stack pointer
+  uint16_t programCounter; // stores the memory address of the statement
+                           // to be executed
 
-        uint8_t delayTimer;     // Used for various timings
-        uint8_t soundTimer;     // Used for sound timing
-        uint8_t keypad[16];     // 16 input keys that match the first 16 hex
-                                // values: 0 - F
+  uint16_t stack[16];   // the 16 level stack
+  uint8_t stackPointer; // 8-bit stack pointer
 
-        uint32_t video[64*32];  // screen
-        uint16_t opcode;
+  uint8_t delayTimer; // Used for various timings
+  uint8_t soundTimer; // Used for sound timing
+  uint8_t keypad[16]; // 16 input keys that match the first 16 hex
+                      // values: 0 - F
 
-        // Member functions
-        void loadROM(const char*);
+  uint32_t video[64 * 32]; // screen
+  uint16_t opcode;
 
+  uint8_t randByte;
+
+  // Member functions
+  Chip8();
+  void loadROM(const char *);
 };
